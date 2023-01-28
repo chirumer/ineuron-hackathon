b7d51ce0b5e13e627d0888f2b8b2f8fd4890e14e
chirumer <imchirumer@gmail.com>
2023-01-28 17:42:03 +0530

    connecting to maps api

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const { Client } = require('@googlemaps/google-maps-services-js')
 
 require('dotenv').config();
 
 app.use(bodyParser.json());
+
+const client = new Client({});
+
+client
+  .elevation({
+    params: {
+      locations: [{ lat: 45, lng: -110 }],
+      key: process.env.GOOGLE_MAPS_API_KEY
+    },
+    timeout: 1000 // milliseconds
+  }, axios.create())
+  .then(r => {
+    console.log(r.data.results[0].elevation);
+  })
+  .catch(e => {
+    console.log(e);
+  });
 
 app.get('/', (req, res) => {
   res.send('Server Active')
@@ -12,7 +31,7 @@ app.get('/', (req, res) => {
 
 app.post('/get_route', (req, res) => {
   const { from, to } = req.body;
-  
+
 });
 
 const PORT = process.env.PORT
