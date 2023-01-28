377089c5eedcfbff247d9e89d5b1413fb937ef74
Ninad Sonawane <77869600+ninadsonawane@users.noreply.github.com>
2023-01-28 17:58:58 +0530

    intergrated alan ai

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+  useEffect(() => {
+    alanBtn({
+        key: 'b8bd8ab891cce7c43cbb48a7f7162e6d2e956eca572e1d8b807a3e2338fdd0dc/stage',
+        onCommand: (commandData) => {
+          if (commandData.command === 'go:back') {
+            // Call the client code that will react to the received command
+          }
+        }
+    });
+  }, []);
+
+  
+  return (
+    <div className="App">
+
+     Ninad Sonawane
+    </div>
+  );
+}
+
+export default App;
